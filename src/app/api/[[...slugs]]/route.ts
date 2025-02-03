32035e8e79e9c0c5f61d3b16dd2f6299059e78e7
Dito Aryaputra Ramadhani <ditoaryaputra1324@gmail.com>
2025-02-03 22:23:12 +0700

    test: get & post elysia

--- src/app/api/[[...slugs]]/route.ts
+++ src/app/api/[[...slugs]]/route.ts
@@ -1,15 +1,36 @@
-import { Elysia, t } from 'elysia'
+import { Elysia, t } from "elysia";
 
-const app = new Elysia({ prefix: '/api' })
-    .get('/', () => 'hello Next')
-    .post('/', ({ body }) => body, {
-        body: t.Object({
-            name: t.String()
-        })
-    })
+const users: any = [];
 
-export const GET = app.handle 
-export const POST = app.handle 
-export const PUT = app.handle 
-export const PATCH = app.handle 
-export const DELETE = app.handle 
+const app = new Elysia({ prefix: "/api" })
+  .get("/", () => ({
+    message: "hello Next",
+  }))
+  .post("/", ({ body }) => body, {
+    body: t.Object({
+      name: t.String(),
+    }),
+  });
+
+app.get("/names", () => ({
+  name: users,
+}));
+
+app.post(
+  "/name",
+  ({ body }) => {
+    users.push(body.name);
+    return { success: true, users };
+  },
+  {
+    body: t.Object({
+      name: t.String(),
+    }),
+  }
+);
+
+export const GET = app.handle;
+export const POST = app.handle;
+export const PUT = app.handle;
+export const PATCH = app.handle;
+export const DELETE = app.handle;
