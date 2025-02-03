c72c9c8866feede255b7718ec973ceff03650ab4
Dito Aryaputra Ramadhani <ditoaryaputra1324@gmail.com>
2025-02-03 22:08:48 +0700

    feat: add elysia api

--- src/app/api/[[...slugs]]/route.ts
+++ src/app/api/[[...slugs]]/route.ts
@@ -10,3 +10,6 @@ const app = new Elysia({ prefix: '/api' })
 
 export const GET = app.handle 
 export const POST = app.handle 
+export const PUT = app.handle 
+export const PATCH = app.handle 
+export const DELETE = app.handle 
